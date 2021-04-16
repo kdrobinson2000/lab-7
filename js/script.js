88ae44bff1cc7ac2792bce4afcd897c1b27b9332
Kimberly Robinson <krobinson5@hawk.iit.edu>
2021-04-16 13:47:18 -0500

    No Changes made to file.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+<script type= "text/javascript">
+function init(){
+//add your javascrip between these two lines of code
+alert('Hello Kim');	
+}
+ 	
+var button = document.getElementById ('entrybutton');
+button.addEventListener ('click', init);
+
+
+//window.addEventListener('load', init);
+</script>
---
+++ script.js
@@ -0,0 +1,8 @@
+<script type= "text/javascript">
+function init(){
+alert('Hello Kim');
+}
+var button = document.getElementById('entrybutton');
+button.addEventListener ('click', init);
+</script>
+//window.addEventListener('load', init);
