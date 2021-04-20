937d48a32156b2b301b927249d0e26258169acf5
Kimberly Robinson <krobinson5@hawk.iit.edu>
2021-04-20 15:18:19 -0500

    Moved Script File to JS Folder

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-
-<script>
 function init(){
-alert(input.value);
+  alert(input.value);
+  output.innerHTML = input.value; 
 }
 
 var button = document.getElementById ('entrybutton');
-
+var input = document.getElementById ('entryinput');
+var output = document.getElementById ('textoutput');
 
 button.addEventListener ('click', init);
-
-
-window.addEventListener('load', init);
-</script>
+window.addEventListener ('load', init);
